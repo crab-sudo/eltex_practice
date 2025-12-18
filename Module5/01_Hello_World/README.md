c36f2745a45a3213c1c41cd3d51e160a1e96f0c6
crab-sudo <ozucoya@mail.ru>
2025-12-18 14:47:40 +0700

    Выполнено задание 01_Hello_world

---
+++ Module5/01_Hello_World/README.md
@@ -0,0 +1,11 @@
+![[Pasted image 20251218143956.png]]
+В самом низу  вывелось  "Hello, world".
+
+![[Pasted image 20251218144220.png]]
+Модуль "hello" загружен в ядро.
+
+![[Pasted image 20251218144336.png]]
+В самом низу вывелось "Goodbye, world"
+
+![[Pasted image 20251218144455.png]]
+Модуль "hello" выгружен из ядра
---
+++ Module5/01_Hello_World/hello.c
@@ -0,0 +1,20 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("MyLicense");
+MODULE_AUTHOR("Evgeniy");
+MODULE_DESCRIPTION("Hello_world kernel module");
+MODULE_VERSION("0.1");
+
+static int __init hello_init(void){
+    printk(KERN_INFO "Hello, world\n");
+    return 0;
+}
+
+static void __exit hello_exit(void){
+    printk(KERN_INFO "Goodbye, world\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
